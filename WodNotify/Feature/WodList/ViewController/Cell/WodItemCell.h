b794ec3f4cb02e446f1a428d8ee0408984bdfb0b
Tyler Madonna <developer@madonnaapps.com>
2019-12-30 15:07:06 -0500

    Added WodItemCell table view cell

---
+++ WodNotify/Feature/WodList/ViewController/Cell/WodItemCell.h
@@ -0,0 +1,17 @@
+//
+//  WodItemCell.h
+//  WodNotify
+//
+//  Created by Tyler Madonna on 12/30/19.
+//  Copyright © 2019 Tyler Madonna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WodItemCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WodNotify/Feature/WodList/ViewController/Cell/WodItemCell.m
@@ -0,0 +1,24 @@
+//
+//  WodItemCell.m
+//  WodNotify
+//
+//  Created by Tyler Madonna on 12/30/19.
+//  Copyright © 2019 Tyler Madonna. All rights reserved.
+//
+
+#import "WodItemCell.h"
+
+@implementation WodItemCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
