4c3f8abb1714170c8debf4616af6310c04990b65
Tyler Madonna <developer@madonnaapps.com>
2019-12-31 17:24:06 -0500

    Setup tableview in WodListViewController

--- WodNotify/Feature/WodList/ViewController/WodListViewController.m
+++ WodNotify/Feature/WodList/ViewController/WodListViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "WodListViewController.h"
+#import "WodItemCell.h"
 
 @interface WodListViewController ()
 
 @property (strong, nonatomic) id<WodListPresenterProtocol> presenter;
+
+@property (strong, nonatomic) NSArray<WodListViewItem *> * wodItems;
 
 @end
 
@@ -27,24 +30,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupTableView];
     [self.presenter setView:self];
     self.view.backgroundColor = [UIColor purpleColor];
+}
+
+- (void)setupTableView {
+    UINib *nib = [UINib nibWithNibName:kWodItemCellNibName bundle:[NSBundle mainBundle]];
+    [self.tableView registerNib:nib forCellReuseIdentifier:kWodItemCellCellId];
 }
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 0;
+    return self.wodItems ? self.wodItems.count : 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    WodItemCell *cell = [tableView dequeueReusableCellWithIdentifier:kWodItemCellCellId forIndexPath:indexPath];
+    WodListViewItem *wodItem = self.wodItems[indexPath.item];
+    [cell setWodItem:wodItem];
+    return cell;
 }
 
 #pragma mark - WodListView
 
 - (void)presentWodList:(NSArray<WodListViewItem *> *)wodList {
-
+    self.wodItems = wodList;
+    NSLog(@"Presenting %lu items", wodList.count);
+    [self.tableView reloadData];
 }
 
 @end
