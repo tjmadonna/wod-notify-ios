e257372d16944a8ef1e5f2c37507860e7154c82b
Tyler Madonna <developer@madonnaapps.com>
2020-01-02 21:05:32 -0500

    Added syncNewWodsWithCompletion to SyncDataManagerProtocol

---
+++ WodNotify/Data/DataManager/SyncDataManagerProtocol.h
@@ -0,0 +1,27 @@
+//
+//  SyncDataManagerProtocol.h
+//  WodNotify
+//
+//  Created by Tyler Madonna on 1/2/20.
+//  Copyright © 2020 Tyler Madonna. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+#import "WodModel.h"
+
+#ifndef SyncDataManagerProtocol_h
+#define SyncDataManagerProtocol_h
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef void (^NewWodCompletion)( NSArray<WodModel *> * _Nullable , NSError * _Nullable );
+
+@protocol SyncDataManagerProtocol <NSObject>
+
+- (void)syncNewWodsWithCompletion:(NewWodCompletion)completion;
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+
+#endif /* SyncDataManagerProtocol_h */
