e7913236e8f713baa1a069c5f4d3dbb602f482ad
Tyler Madonna <developer@madonnaapps.com>
2020-01-02 19:44:08 -0500

    Changed getAllWodsWithCompletion in CoreDataManager to return an array of WodModel

--- WodNotify/Data/Local/CoreDataManager.m
+++ WodNotify/Data/Local/CoreDataManager.m
@@ -7,6 +7,7 @@
 //
 
 #import "CoreDataManager.h"
+#import "WodLocalModel+CoreDataProperties.h"
 
 @interface CoreDataManager ()
 
@@ -27,20 +28,44 @@
 - (void)getAllWodsWithCompletion:(nonnull WodQueryCompletion)completion {
     [self.persistentContainer.viewContext performBlock:^{
 
-        NSFetchRequest *fetchRequest = [WodEntity fetchRequest];
+        NSFetchRequest *fetchRequest = [WodLocalModel fetchRequest];
         fetchRequest.predicate = [NSPredicate predicateWithValue:YES];
         fetchRequest.sortDescriptors = @[ [[NSSortDescriptor alloc] initWithKey:@"date" ascending:YES] ];
 
         NSError *error;
-        NSArray<WodEntity *> *wods = [self.persistentContainer.viewContext executeFetchRequest:fetchRequest error:&error];
+        NSArray<WodLocalModel *> *wods = [self.persistentContainer.viewContext executeFetchRequest:fetchRequest
+                                                                                             error:&error];
 
         if (error) {
             completion(nil, error);
             return;
         }
 
-        completion(wods, nil);
+        NSArray<WodModel *> *wodModels = [self mapLocalWodModelArrayToWodModelArray:wods];
+
+        completion(wodModels, nil);
     }];
+}
+
+- (NSArray<WodModel *> *)mapLocalWodModelArrayToWodModelArray:(NSArray<WodLocalModel *> *)localWodModelArray {
+    NSMutableArray<WodModel *> *wodModelArray = [[NSMutableArray alloc] initWithCapacity:localWodModelArray.count];
+
+    for (WodLocalModel *wodLocalModel in localWodModelArray) {
+
+        if (wodLocalModel.uid && wodLocalModel.date &&
+            wodLocalModel.title && wodLocalModel.author &&
+            wodLocalModel.url && wodLocalModel.summary) {
+
+            [wodModelArray addObject: [[WodModel alloc] initWithUid:wodLocalModel.uid
+                                                               date:wodLocalModel.date
+                                                              title:wodLocalModel.title
+                                                             author:wodLocalModel.author
+                                                                url:wodLocalModel.url
+                                                            summary:wodLocalModel.summary]];
+        }
+    }
+
+    return wodModelArray;
 }
 
 @end
