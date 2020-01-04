ac8fb28890580d84822755bf9159c23c618a805d
Tyler Madonna <developer@madonnaapps.com>
2020-01-04 18:02:57 -0500

    Registered notifications and wod sync background task in SceneDelegate

--- WodNotify/Scene/SceneDelegate.m
+++ WodNotify/Scene/SceneDelegate.m
@@ -1,11 +1,11 @@
 #import "SceneDelegate.h"
 #import "ApplicationRouter.h"
 
-#import <BackgroundTasks/BackgroundTasks.h>
-
 @interface SceneDelegate ()
 
 @property (strong, nonatomic) ApplicationRouter * appRouter;
+
+@property (strong, nonatomic) ApplicationComponent * appComponent;
 
 @end
 
@@ -18,44 +18,16 @@
     [self.window makeKeyAndVisible];
     self.window.windowScene = (UIWindowScene *)scene;
 
-    ApplicationComponent *applicationComponent = [ApplicationComponent shared];
-    self.appRouter = [[ApplicationRouter alloc] initWithWindow:self.window applicationComponent:applicationComponent];
+    self.appComponent = [ApplicationComponent sharedComponent];
+    self.appRouter = [[ApplicationRouter alloc] initWithWindow:self.window applicationComponent:self.appComponent];
     [self.appRouter navigateToWodListViewController];
 
-    [self registerBackgroundTask];
+    [self.appComponent.notificationManager requestPermissions];
+    [self.appComponent.backgroundTaskManager registerWodSyncBackgroundTask];
 }
 
 - (void)sceneDidEnterBackground:(UIScene *)scene {
-    [self scheduleBackgroundTask];
-}
-
-#pragma mark - Background tasks
-
-- (void)registerBackgroundTask {
-    [[BGTaskScheduler sharedScheduler] registerForTaskWithIdentifier:@"com.madonnaapps.WodNotify.Refresh"
-                                                          usingQueue:dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0) launchHandler:^(__kindof BGTask * _Nonnull task) {
-        [self executeBackgroundTask:task];
-        [self scheduleBackgroundTask];
-    }];
-}
-
-- (void)scheduleBackgroundTask {
-    BGAppRefreshTaskRequest *request = [[BGAppRefreshTaskRequest alloc] initWithIdentifier:@"com.madonnaapps.WodNotify.Refresh"];
-    request.earliestBeginDate = [[NSDate alloc] initWithTimeIntervalSinceNow:3600];
-    [[BGTaskScheduler sharedScheduler] submitTaskRequest:request error:nil];
-}
-
-- (void)executeBackgroundTask:(BGTask *)task {
-    ApplicationComponent *applicationComponent = [ApplicationComponent shared];
-    [[applicationComponent syncDateManager] syncNewWodsWithCompletion:^(NSArray<WodModel *> * _Nullable newWodArray,
-                                                                        NSError * _Nullable error) {
-        if (error) {
-            [task setTaskCompletedWithSuccess:NO];
-            return;
-        }
-
-        [task setTaskCompletedWithSuccess:YES];
-    }];
+    [self.appComponent.backgroundTaskManager scheduleWodSyncBackgroundTask];
 }
 
 @end
